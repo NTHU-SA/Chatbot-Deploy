ae52f90c49e759035a77b94938d3cb027263456e
lawrence910426 <lawrence910426@gmail.com>
2022-02-25 01:18:41 +0800

    Database imported & Successsfully running
    Guidelines added

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Chatbot-Deploy
+
+# How to build your own version
+- Nginx.conf (NTHU-Chatbot)
+    - location/api set ip to the one represents host machiine
+- mongo-import.sh (Chatbot-Deploy)
+    - set $host to the ip that represents host machine
+- mongodb.go (NTHU-Chatbot-API)
+    - set ip to the one represents host machine
---
+++ mongo-import.sh
@@ -0,0 +1,19 @@
+host=192.168.106.44
+mongoimport --host $host --db nthu_chatbot_db --collection academic_phone --jsonArray --file /mnt/academic_phone.json
+mongoimport --host $host --db nthu_chatbot_db --collection admin --jsonArray --file /mnt/admin.json
+mongoimport --host $host --db nthu_chatbot_db --collection administration_phone --jsonArray --file /mnt/administration_phone.json
+mongoimport --host $host --db nthu_chatbot_db --collection anecdote --jsonArray --file /mnt/anecdote.json
+mongoimport --host $host --db nthu_chatbot_db --collection bus_schedule --jsonArray --file /mnt/bus_schedule.json
+mongoimport --host $host --db nthu_chatbot_db --collection bus_schedule_summer --jsonArray --file /mnt/bus_schedule_summer.json
+mongoimport --host $host --db nthu_chatbot_db --collection epidemic_data --jsonArray --file /mnt/epidemic_data.json
+mongoimport --host $host --db nthu_chatbot_db --collection feedback --jsonArray --file /mnt/feedback.json
+mongoimport --host $host --db nthu_chatbot_db --collection instanews --jsonArray --file /mnt/instanews.json
+mongoimport --host $host --db nthu_chatbot_db --collection ocamp --jsonArray --file /mnt/ocamp.json
+mongoimport --host $host --db nthu_chatbot_db --collection ocamp_checkpoint --jsonArray --file /mnt/ocamp_checkpoint.json
+mongoimport --host $host --db nthu_chatbot_db --collection ocamp_course --jsonArray --file /mnt/ocamp_course.json
+mongoimport --host $host --db nthu_chatbot_db --collection ocamp_game --jsonArray --file /mnt/ocamp_game.json
+mongoimport --host $host --db nthu_chatbot_db --collection ocamp_group --jsonArray --file /mnt/ocamp_group.json
+mongoimport --host $host --db nthu_chatbot_db --collection qa_data --jsonArray --file /mnt/qa_data.json
+mongoimport --host $host --db nthu_chatbot_db --collection token --jsonArray --file /mnt/token.json
+mongoimport --host $host --db nthu_chatbot_db --collection user --jsonArray --file /mnt/user.json
+mongoimport --host $host --db nthu_chatbot_db --collection user_map_record --jsonArray --file /mnt/user_map_record.json
